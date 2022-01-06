b7563231927216f5edecc2daeabe785fd2259575
David Ogborn <ogbornd@mcmaster.ca>
2022-01-06 13:03:21 -0500

    added eval button

--- src/Main.purs
+++ src/Main.purs
@@ -19,29 +19,32 @@ import RenderEngine
 import Program
 
 
-type EditorState = String -- just an error message for now
+type EditorState = { text :: String, status :: String }
 
 initialState :: forall input. input -> EditorState
-initialState _ = ""
+initialState _ = { text: "", status: "" }
 
-type EditorAction = String -- represents text to be evaluated/parsed
+data EditorAction = TextChanged String | Evaluate
 
 render :: forall m. EditorState -> H.ComponentHTML EditorAction () m
 render st = H.div [ H.class_ $ H.ClassName "editor" ]
   [
     H.div [] [H.text "untitled NFRF project"],
-    H.div [] [H.textarea [ H.onValueInput identity ]],
-    H.div [] [H.text st]
+    H.div [] [H.textarea [ H.onValueInput TextChanged ]],
+    H.div [] [H.button [ H.onClick $ \_ -> Evaluate] [ H.text "eval"] ],
+    H.div [] [H.text st.status]
   ]
 
 handleAction :: forall output m. MonadEffect m => RenderEngine -> EditorAction -> H.HalogenM EditorState EditorAction () output m Unit
-handleAction re txt = do
-  case parseProgram txt of
-    Just p -> do
-      H.liftEffect $ setProgram re p
-      H.modify_ $ \_ -> "success!"
-    Nothing ->
-      H.modify_ $ \_ -> "unable to parse"
+handleAction re ea = case ea of
+  TextChanged t -> H.modify_ $ \st -> st { text = t }
+  Evaluate -> do
+    st <- H.get
+    case parseProgram st.text of
+      Just p -> do
+        H.liftEffect $ setProgram re p
+        H.modify_ _ { status = "success!" }
+      Nothing -> H.modify_ _ { status = "unable to parse" }
 
 component :: forall query input output m. MonadEffect m => RenderEngine -> H.Component query input output m
 component re =
