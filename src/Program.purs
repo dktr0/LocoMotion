b57e0d25ecc373a2a07b96e6b7df9d9e42cfb1a2
David Ogborn <ogbornd@mcmaster.ca>
2022-01-12 16:44:31 -0500

    simple parser working

--- src/Program.purs
+++ src/Program.purs
@@ -1,12 +1,135 @@
-module Program where
+module Program (
+  Program(..),
+  defaultProgram,
+  parseProgram
+  ) where
 
+import Prelude
+import Data.Identity
+import Data.List
+import Data.List.NonEmpty
+import Data.Either
 import Data.Maybe (Maybe)
 import Data.Number (fromString)
+import Control.Bind
+import Text.Parsing.Parser
+import Text.Parsing.Parser.Pos
+import Text.Parsing.Parser.Language (emptyDef)
+import Text.Parsing.Parser.Token (GenLanguageDef(..),LanguageDef,unGenLanguageDef,TokenParser,GenTokenParser,makeTokenParser)
+import Text.Parsing.Parser.Combinators
 
-type Program = Number -- for now, a valid program is simply a number controlling speed of rotation
+
+data Program = Stationary | Fast | Slow
 
 defaultProgram :: Program
-defaultProgram = 0.0
+defaultProgram = Stationary
 
-parseProgram :: String -> Maybe Program
-parseProgram = fromString
+parseProgram :: String -> Either String Program
+parseProgram x = case (runParser x program) of
+  Left err -> Left $ showParseError err
+  Right prog -> Right prog
+
+showParseError :: ParseError -> String
+showParseError (ParseError e (Position p)) = e -- placeholder
+-- Position { column :: Int, line :: Int }
+
+type P a = ParserT String Identity a
+
+program :: P Program
+program = choice [
+  reserved "stationary" $> Stationary,
+  reserved "fast" $> Fast,
+  reserved "slow" $> Slow
+  ]
+
+tokenParser :: GenTokenParser String Identity
+tokenParser = makeTokenParser $ LanguageDef (unGenLanguageDef emptyDef) {
+  reservedNames = ["stationary","fast","slow"]
+  }
+
+  {-  LanguageDef { caseSensitive :: Boolean, commentEnd :: String, commentLine :: String, commentStart :: String, identLetter :: ParserT s m Char, identStart :: ParserT s m Char, nestedComments :: Boolean, opLetter :: ParserT s m Char, opStart :: ParserT s m Char, reservedNames :: Array String, reservedOpNames :: Array String } -}
+
+
+angles :: forall a. P a -> P a
+angles = tokenParser.angles
+
+braces :: forall a. P a -> P a
+braces = tokenParser.braces
+
+brackets :: forall a. P a -> P a
+brackets = tokenParser.brackets
+
+charLiteral :: P Char
+charLiteral = tokenParser.charLiteral
+
+colon :: P String
+colon = tokenParser.colon
+
+comma :: P String
+comma = tokenParser.comma
+
+commaSep :: forall a. P a -> P (List a)
+commaSep = tokenParser.commaSep
+
+commaSep1 :: forall a. P a -> P (NonEmptyList a)
+commaSep1 = tokenParser.commaSep1
+
+decimal :: P Int
+decimal = tokenParser.decimal
+
+dot :: P String
+dot = tokenParser.dot
+
+float :: P Number
+float = tokenParser.float
+
+hexadecimal :: P Int
+hexadecimal = tokenParser.hexadecimal
+
+identifier :: P String
+identifier = tokenParser.identifier
+
+integer :: P Int
+integer = tokenParser.integer
+
+lexeme :: forall a. P a -> P a
+lexeme = tokenParser.lexeme
+
+natural :: P Int
+natural = tokenParser.natural
+
+naturalOrFloat :: P (Either Int Number)
+naturalOrFloat = tokenParser.naturalOrFloat
+
+octal :: P Int
+octal = tokenParser.octal
+
+operator :: P String
+operator = tokenParser.operator
+
+parens :: forall a. P a -> P a
+parens = tokenParser.parens
+
+reserved :: String -> P Unit
+reserved = tokenParser.reserved
+
+reservedOp :: String -> P Unit
+reservedOp = tokenParser.reservedOp
+
+semi :: P String
+semi = tokenParser.semi
+
+semiSep :: forall a. P a -> P (List a)
+semiSep = tokenParser.semiSep
+
+semiSep1 :: forall a. P a -> P (NonEmptyList a)
+semiSep1 = tokenParser.semiSep1
+
+stringLiteral :: P String
+stringLiteral = tokenParser.stringLiteral
+
+symbol :: String -> P String
+symbol = tokenParser.symbol
+
+whiteSpace :: P Unit
+whiteSpace = tokenParser.whiteSpace
