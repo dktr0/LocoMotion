5fb52101f9e71e975beb808c27d5dc3fb6b86d9c
David Ogborn <ogbornd@mcmaster.ca>
2022-06-14 17:49:09 -0400

    url changes in dancers work

--- src/src/DancerState.purs
+++ src/src/DancerState.purs
@@ -24,17 +24,29 @@ type MaybeRef a = Ref (Maybe a)
 
 type DancerState =
   {
+  url :: Ref String,
   theDancer :: MaybeRef Three.Scene,
   animations :: MaybeRef (Array Three.AnimationClip),
   animationMixer :: MaybeRef Three.AnimationMixer
   }
 
 
+newDancerState :: String -> Effect DancerState
+newDancerState x = do
+  url <- new x
+  theDancer <- new Nothing
+  animations <- new Nothing
+  animationMixer <- new Nothing
+  pure { url, theDancer, animations, animationMixer }
+
+
 runDancerWithState :: Three.Scene -> Number -> Dancer -> Maybe DancerState -> Effect DancerState
 runDancerWithState theScene nCycles d maybeDancerState = do
   dState <- case maybeDancerState of
     Nothing -> addDancer theScene d
-    Just x -> pure x
+    Just x -> do
+      updateModelIfNecessary theScene d x
+      pure x
   ms <- read dState.theDancer
   case ms of
     Just s -> do
@@ -61,24 +73,38 @@ runDancerWithState theScene nCycles d maybeDancerState = do
 
 addDancer :: Three.Scene -> Dancer -> Effect DancerState
 addDancer theScene d = do
-  theDancer <- new Nothing
-  animations <- new Nothing
-  animationMixer <- new Nothing
-  let url' = resolveURL d.url
+  dState <- newDancerState d.url
+  loadModel theScene d.url dState
+  pure dState
+
+
+updateModelIfNecessary :: Three.Scene -> Dancer -> DancerState -> Effect Unit
+updateModelIfNecessary theScene d dState = do
+  let urlProg = d.url
+  urlState <- read dState.url
+  when (urlProg /= urlState) $ do
+    removeDancer theScene dState
+    loadModel theScene d.url dState
+
+
+loadModel :: Three.Scene -> String -> DancerState -> Effect Unit
+loadModel theScene url dState = do
+  write url dState.url
+  let url' = resolveURL url
   _ <- Three.loadGLTF_DRACO "https://dktr0.github.io/LocoMotion/threejs/" url' $ \gltf -> do
     log $ "model loaded with " <> show (length gltf.animations) <> " animations"
     Three.addAnything theScene gltf.scene
-    write (Just gltf.scene) theDancer
-    write (Just gltf.animations) animations
+    write (Just gltf.scene) dState.theDancer
+    write (Just gltf.animations) dState.animations
     animMixer <- Three.newAnimationMixer gltf.scene
-    write (Just animMixer) animationMixer
+    write (Just animMixer) dState.animationMixer
     case gltf.animations!!0 of
       Just a -> do
         defaultAction <- Three.clipAction animMixer a
         Three.setEffectiveTimeScale defaultAction 1.0
         Three.playAnything defaultAction
       Nothing -> pure unit
-  pure { theDancer, animations, animationMixer }
+  pure unit
 
 
 removeDancer :: Three.Scene -> DancerState -> Effect Unit
