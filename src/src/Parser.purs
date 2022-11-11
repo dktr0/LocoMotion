bd9d2887d2a159248bd58ca1ea76142e47a126bb
David Ogborn <ogbornd@mcmaster.ca>
2022-11-11 15:50:54 -0500

    expressionToAction added to Parser

--- src/src/Parser.purs
+++ src/src/Parser.purs
@@ -1,6 +1,37 @@
 module Parser where
 
-{- parseProgram :: String -> Either String Program
+import Prelude
+import Data.Map
+
+import Value
+import AST (AST)
+import AST as AST
+
+type Program = Map Int Action
+
+data Action =
+  Dancer Transformer |
+  Floor Transformer |
+  Camera Transformer
+
+{-
+astToProgram :: AST -> Either ParseError Program
+astToProgram ast = do
+  -- calculate map of semi-globals
+  -- use that
+-}
+
+expressionToAction :: ValueMap -> Expression -> Either ParseErrror Action
+expressionToAction semiMap e = do
+  v <- expressionToValue semiMap empty e
+  case v of
+    ValueDancer t -> pure $ Dancer t
+    ValueFloor t -> pure $ Floor t
+    ValueCamera t -> pure $ Camera t
+    _ -> throwError $ ParseError "an Action must be a Dancer, Floor, or Camera" (expressionPosition e)
+
+{-
+parseProgram :: String -> Either String Program
 parseProgram x = case (runParser x program) of
   Left err -> Left $ showParseError err
   Right prog -> Right prog
